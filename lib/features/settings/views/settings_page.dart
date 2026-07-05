6d3d81be45a90f29359c6141d4a657fdb965fdbb
Shashi Singh <alphasingh8434@gmail.com>
2026-07-05 13:21:19 +0530

    fix: added cached_network_image to remove delay in profile image fetch for drawer and settings page

--- lib/features/settings/views/settings_page.dart
+++ lib/features/settings/views/settings_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import '../../../app/router/app_routes.dart';
 import '../../../app/theme/theme_notifier.dart';
 import '../../auth/controllers/auth_controller.dart';
@@ -54,7 +55,7 @@ class _SettingsPageState extends ConsumerState<SettingsPage> {
               ListTile(
                 leading: CircleAvatar(
                   backgroundImage: user?.photoURL != null
-                      ? NetworkImage(user!.photoURL!)
+                      ? CachedNetworkImageProvider(user!.photoURL!)
                       : null,
                   backgroundColor: colorScheme.primary,
                   child: user?.photoURL == null
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,6 +14,7 @@ import google_sign_in_ios
 import package_info_plus
 import record_darwin
 import shared_preferences_foundation
+import sqflite_darwin
 import sqlite3_flutter_libs
 import url_launcher_macos
 
@@ -27,6 +28,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
